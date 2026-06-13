22693d6e6df63403540b75534ca29eca84015072
Tohidmul786 <160647256+Tohidmul786@users.noreply.github.com>
2026-06-13 12:07:52 +0530

    flat_views.py

--- apartment_maintenance/views/flat_views.py
+++ apartment_maintenance/views/flat_views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 from apartment_maintenance.models import Flat, FlatOwner, Wing, Floor
 from apartment_maintenance.serializers import (
